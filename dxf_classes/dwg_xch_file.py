6fa65168e68dec6ca36663513c8101a6103e36bd
amwaeber <amwaeber@gmail.com>
2018-03-05 16:39:31 +0100

    using ezdxf for reading/manipulating/writing dxf

--- dxf_classes/dwg_xch_file.py
+++ dxf_classes/dwg_xch_file.py
@@ -1,6 +1,5 @@
 from PyQt5 import QtWidgets
 
-import ezdxf
 import dxfgrabber
 import io
 from dxfwrite import DXFEngine as dxf
@@ -14,7 +13,7 @@ class DwgXchFile:
     def __init__(self):
         self.file_name = ''
         self.file_type = 'standard'
-        self.drawing = ezdxf.new('R2010')
+        self.drawing = dxfgrabber.read(io.StringIO())
 
     def load(self, parent, file_type='standard', **kwargs):
         self.file_name = kwargs.get('file_name', self.file_name)
@@ -35,7 +34,7 @@ class DwgXchFile:
         if not fname:  # capture cancel in dialog
             return
         self.file_name = fname
-        self.drawing = ezdxf.readfile(self.file_name)
+        self.drawing = dxfgrabber.readfile(self.file_name)
 
     def save(self, parent, overwrite=True):
         if any([not overwrite, not self.file_name, not self.file_type == 'standard']):
@@ -45,6 +44,33 @@ class DwgXchFile:
                 return
             self.file_name = fname
             self.file_type = 'standard'
-            self.drawing.saveas(self.file_name)
-        else:
-            self.drawing.save()
+        save_drawing = dxf.drawing(self.file_name)
+        for l in self.drawing.layers:
+            save_drawing.add_layer(name=l.name, color=l.color, linetype=l.linetype)
+        for s in self.drawing.styles:
+            save_drawing.add_style(name=s.name, height=s.height, width=s.width, oblique=s.oblique,
+                                   font=s.font, bigfont=s.big_font)
+        for e in self.drawing.entities:
+            if e.dxftype == 'LINE':
+                line = dxf.line(start=e.start, end=e.end, layer=e.layer, color=e.color)
+                save_drawing.add(line)
+            elif e.dxftype == 'CIRCLE':
+                circle = dxf.circle(radius=e.radius, center=e.center, layer=e.layer, color=e.color)
+                save_drawing.add(circle)
+            elif e.dxftype == 'POLYLINE':
+                pline = dxf.polyline(points=e.points, layer=e.layer, color=e.color)
+                if e.is_closed:
+                    pline.close()
+                save_drawing.add(pline)
+            elif e.dxftype == 'TEXT':
+                if e.is_backwards:
+                    m_flag = const.MIRROR_X
+                elif e.is_upside_down:
+                    m_flag = const.MIRROR_Y
+                else:
+                    m_flag = 0
+                text = dxf.circle(text=e.text, insert=e.insert, height=e.height, width=e.width,
+                                  oblique=e.oblique, rotation=e.rotation, style=e.style, halign=e.halign,
+                                  valign=e.valign, mirror=m_flag, layer=e.layer, color=e.color)
+                save_drawing.add(text)
+        save_drawing.save()
